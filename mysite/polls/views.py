b0d8477375d49af4884ff1efa11b71fe96fde04c
sh-chenyk <sh-chenyk@chinaunicom.cn>
2019-01-16 15:04:26 +0800

    接口功能

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@
 
 # 于是 Django 提供了一个快捷函数render，载入模板，填充上下文，再返回由它生成的 HttpResponse 对象。
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.urls import reverse
 from django.views import generic
 # 加载模板
@@ -93,6 +93,5 @@ def video(request):
 	return HttpResponse(template.render(context, request))
 
 # json测试
-def jsonDemo(request, question_id=1):
-	question = Question
-	return HttpResponse(question)
+def jsonDemo(request):
+	return JsonResponse({"result": 0, "msg": "执行成功"})
